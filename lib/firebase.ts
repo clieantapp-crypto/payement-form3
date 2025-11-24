ca4f57355da6c486cda6511c715493be02f49797
clieantapp-crypto <clieantapp@gmail.com>
2025-11-24 18:09:59 +0300

    sdc

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { doc, getFirestore, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
- apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCOb7p6XhHRZ5zfejrkiP4bUWStEibE_K4",
+  authDomain: "payment-form-f8df2.firebaseapp.com",
+  projectId: "payment-form-f8df2",
+  storageBucket: "payment-form-f8df2.firebasestorage.app",
+  messagingSenderId: "216576356738",
+  appId: "1:216576356738:web:1c688e06370a4ec63c9f9b",
+  measurementId: "G-4NQ81F6W8Y"
 };
 
 const app =  initializeApp(firebaseConfig);
